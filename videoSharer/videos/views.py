ba43a23ab1e0f0a987150398b20ceadb687d1563
Andrew Monakov <monakow.andrew@yandex.ru>
2021-11-22 00:10:26 +0300

    Index commmit
    Добавление главной страницы и ссылок на вкладки

--- videoSharer/videos/views.py
+++ videoSharer/videos/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, reverse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Video
 
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-def index(request):
-	return render(request, 'videos/index.html')
+class Index(ListView):
+	model = Video
+	template_name = 'videos/index.html'
+	order_by = '-date_posted'
 
 class CreateVideo(LoginRequiredMixin, CreateView):
 	model = Video
