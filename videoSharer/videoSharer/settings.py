a5c1cd4fcf2e190fd40fec263093dc8038ea33d3
Andrew Monakov <monakow.andrew@yandex.ru>
2021-11-22 23:02:47 +0300

    Profile commit

--- videoSharer/videoSharer/settings.py
+++ videoSharer/videoSharer/settings.py
@@ -41,6 +41,7 @@ SITE_ID = 1
 
 INSTALLED_APPS = [
     'videos',
+    'profiles',
     "crispy_forms",
 
     'django.contrib.sites',
@@ -153,3 +154,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Sign In Redirect
 LOGIN_REDIRECT_URL = 'index'
+
+# Additions to Sign Up Form
+ACCOUNT_SIGNUP_FORM_CLASS = 'profiles.forms.ProfileForm'
